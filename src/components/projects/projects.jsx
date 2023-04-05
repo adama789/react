740cab56107f1096a9a89285a424c7f459883b36
adama789 <cedrq@tlen.pl>
2023-04-05 12:55:30 +0200

    Add components

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -3,7 +3,31 @@ import './projects.css'
 
 const projects = () => {
   return (
-    <div>projects</div>
+    <div id="projects">
+    <div class="container">
+        <h1 class="sub-title">My <span>Projects</span></h1>
+        <div class="services-list">
+            <div>
+                <i class="fa-brands fa-python"></i>
+                <h2>Projekt 1</h2>
+                <p>W tym projekcie zrobilem bardzo dobre rzeczy</p>
+                <a href={"#header"}>Learn more</a>
+            </div>
+            <div>
+                <i class="fa-solid fa-pepper-hot"></i>
+                <h2>Projekt 2</h2>
+                <p>W tym projekcie zrobilem bardzo dobre rzeczy</p>
+                <a href={"#header"}>Learn more</a>
+            </div>
+            <div>
+                <i class="fa-brands fa-html5"></i>
+                <h2>Projekt 3</h2>
+                <p>W tym projekcie zrobilem bardzo dobre rzeczy</p>
+                <a href={"#header"}>Learn more</a>
+            </div>
+        </div>
+    </div>
+</div>
   )
 }
 
