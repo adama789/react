bde02818a524431e8b27e35c05d368b8d0034a2a
adama789 <cedrq@tlen.pl>
2023-04-05 12:33:00 +0200

    Add components

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import './header.css'
+import LOGO from '../../assets/logo.png'
 
 const header = () => {
   return (
-    <div>header</div>
+    <div id="header">
+    <div class="container">   
+        <nav>
+            <img src={LOGO} alt="logo" class="logo"/>
+            <ul>
+                <li><a href="#header">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="images/Adam_Cedro_CV.pdf" download >CV</a></li>
+            </ul>
+        </nav>
+        <div class="header-text">
+            <p>Software Developer</p>
+            <h1>Hi, I'm <span>Adam Cedro</span></h1>
+            <div class="social-icons">
+                <a href="https://github.com/adama789" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-github"></i></a>
+                <a href="https://www.linkedin.com/in/adam-cedro-53427025b/" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-linkedin"></i></a>
+                <a href="mailto:cedrq@tlen.pl"><i class="fa-solid fa-envelope"></i></a>
+                <a href="https://www.facebook.com/adam.cedro.39/" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-facebook"></i></a>
+            </div>
+        </div>
+    </div>
+</div>
   )
 }
 
