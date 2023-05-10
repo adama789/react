fe81d1e3252ee1a3105bcd1974670accb92ee687
adama789 <cedrq@tlen.pl>
2023-05-10 22:59:38 +0200

    Update Project components

--- src/pages/AboutPage/components/AboutContent/AboutContent.jsx
+++ src/pages/AboutPage/components/AboutContent/AboutContent.jsx
@@ -4,8 +4,6 @@ const AboutContent = () => {
     return <>
         <h1 className="sub-title">About <span>Me</span></h1>
         <p className="text">
-            I specialize in Python <i className="fa-brands fa-python"></i> language and technologies such as Flask and
-            Django.
             <br></br>My passion for programming allows me to constantly expand my qualifications and experience,<br/>
             as well as provide innovative solutions.
             <br></br>I am committed to creating high-quality, user-friendly applications <br/>that
