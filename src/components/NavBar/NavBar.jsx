660356fa77e5796902dcb30e353d548f3178b737
adama789 <cedrq@tlen.pl>
2023-12-31 01:09:20 +0100

    CV

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = () => {
             <li><a href="#header">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#projects">Projects</a></li>
-            <li><a href={CV} download>CV</a></li>
+            <li><a href={CV}>CV</a></li>
         </ul>
     </nav>;
 }
