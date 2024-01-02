6c2fd3e3f5c55194c68a9dca1adf71861621d4cb
Jakub Pawłowski <59766830+pawl0wski@users.noreply.github.com>
2024-01-02 23:55:06 +0100

    Fix slider

--- src/pages/ProjectsPage/Projects/Project01.jsx
+++ src/pages/ProjectsPage/Projects/Project01.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import './Project.css';
+import React, { useState } from "react";
+import "./Project.css";
 
 const Project01 = () => {
   const [activeSlide, setActiveSlide] = useState(1);
@@ -8,42 +8,53 @@ const Project01 = () => {
     setActiveSlide(slideNumber);
   };
 
-  
+  function calculateLeftOffset(slideNumber) {
+    return `-${(activeSlide - 1) * 100}%`;
+  }
+
   return (
     <section className="container2">
-      <div className="slider-wrapper">
-        <div className="slider">
+      <div className="slider">
+        <div
+          className="slider__images"
+          style={{ marginLeft: calculateLeftOffset(activeSlide) }}
+        >
           <img
             id="slide-1"
             src="https://picsum.photos/seed/picsum/1920/1080"
             alt=""
-            className={activeSlide === 1 ? 'active' : ''}
           />
           <img
             id="slide-2"
             src="https://picsum.photos/seed/img3/1920/1080"
             alt=""
-            className={activeSlide === 2 ? 'active' : ''}
           />
           <img
             id="slide-3"
             src="https://picsum.photos/seed/img2/1920/1080"
             alt=""
-            className={activeSlide === 3 ? 'active' : ''}
           />
         </div>
-
-        <div className="slider-nav">
-          <a href="#slide-1" className={activeSlide === 1 ? 'active' : ''} onClick={(e) => handleSlideClick(1, e)}> </a>
-          <a href="#slide-2" className={activeSlide === 2 ? 'active' : ''} onClick={(e) => handleSlideClick(2, e)}> </a>
-          <a href="#slide-3" className={activeSlide === 3 ? 'active' : ''} onClick={(e) => handleSlideClick(3, e)}> </a>
+        <div className="slider__nav">
+          <div
+            className={activeSlide === 1 ? "active" : ""}
+            onClick={(e) => handleSlideClick(1, e)}
+          ></div>
+          <div
+            className={activeSlide === 2 ? "active" : ""}
+            onClick={(e) => handleSlideClick(2, e)}
+          ></div>
+          <div
+            className={activeSlide === 3 ? "active" : ""}
+            onClick={(e) => handleSlideClick(3, e)}
+          ></div>
         </div>
       </div>
       <div className="header">
-          <h1>Windows Server 2019</h1>
+        <h1>Windows Server 2019</h1>
       </div>
       <div>
-          <p>Lorem</p>
+        <p>Lorem</p>
       </div>
     </section>
   );
