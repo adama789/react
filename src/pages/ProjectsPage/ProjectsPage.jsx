d10130141c03745fc114716acd36601a7602d573
Jakub Pawłowski <59766830+pawl0wski@users.noreply.github.com>
2024-01-02 22:12:57 +0100

    Use <Link> instead of <a>

--- src/pages/ProjectsPage/ProjectsPage.jsx
+++ src/pages/ProjectsPage/ProjectsPage.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
-import './ProjectsPage.css'
+import React from "react";
+import "./ProjectsPage.css";
 import ProjectCard from "./components/ProjectCard/ProjectCard";
 import Title from "../../components/Title/Title";
 import projectsJson from "../../content/projects.json";
 
 const ProjectsPage = () => {
-    const projects = projectsJson.projects;
-    return <div id="projects">
-        <div className="container">
-            <Title>My <span>Projects</span></Title>
-            <div className="services-list">
-                {projects.map((project) => <ProjectCard key={project.name} name={project.name}
-                                                        description={project.description}
-                                                        href={project.href} icon={project.icon}/>)}
-            </div>
+  const projects = projectsJson.projects;
+  return (
+    <div id="projects">
+      <div className="container">
+        <Title>
+          My <span>Projects</span>
+        </Title>
+        <div className="services-list">
+          {projects.map((project) => (
+            <ProjectCard
+              key={project.name}
+              name={project.name}
+              description={project.description}
+              href={project.href}
+              icon={project.icon}
+            />
+          ))}
         </div>
+      </div>
     </div>
-}
+  );
+};
 
-export default ProjectsPage
+export default ProjectsPage;
